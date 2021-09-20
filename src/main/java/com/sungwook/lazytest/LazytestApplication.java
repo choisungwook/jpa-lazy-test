63b81730e576128c999abf744e18a00c00e7e8a8
choisungwook <kgg1959@naver.com>
2021-09-20 14:01:49 +0900

    first commit

---
+++ src/main/java/com/sungwook/lazytest/LazytestApplication.java
@@ -0,0 +1,13 @@
+package com.sungwook.lazytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazytestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sungwook/lazytest/LazytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sungwook.lazytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
