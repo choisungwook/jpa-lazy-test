ea93794aad8069b732b3f31f1f92c47a4e55261d
choisungwook <kgg1959@naver.com>
2021-09-21 16:50:45 +0900

    School, Classroom 연관관계 LAZY 테스트 #5

--- src/test/java/com/sungwook/lazytest/service/ClassroomServiceTest.java
+++ src/test/java/com/sungwook/lazytest/service/ClassroomServiceTest.java
@@ -2,6 +2,8 @@ package com.sungwook.lazytest.service;
 
 import com.sungwook.lazytest.controller.dto.RequestAddSchoolFromClassroomDto;
 import com.sungwook.lazytest.controller.dto.RequestCreateClassroomDTO;
+import com.sungwook.lazytest.entity.ClassRoom;
+import com.sungwook.lazytest.entity.School;
 import com.sungwook.lazytest.repository.ClassroomRepository;
 import com.sungwook.lazytest.repository.SchoolRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -10,6 +12,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,11 +31,20 @@ class ClassroomServiceTest {
     SchoolService schoolService;
 
     @AfterEach
+    @Transactional
     public void AfterEach(){
-        classroomRepository.deleteAll();
-        schoolRepository.deleteAll();
+        System.out.println("JPA Repository 초기화 시작");
+        classroomRepository.deleteAllInBatch();
+        schoolRepository.deleteAllInBatch();
+        System.out.println("JPA Repository 초기화 완료");
     }
 
+    @Test
+    @DisplayName("junit 설정이 잘 완료되었는지 확인")
+    public void Dummy(){
+        
+    }
+    
     @Test
     @DisplayName("반 생성-학교등록X")
     public void CreateClassroom(){
@@ -77,4 +89,59 @@ class ClassroomServiceTest {
         assertDoesNotThrow(() -> classroomService.AddSchool(requset_dto));
     }
 
+    @Test
+    @DisplayName("반 생성과 Lazy쿼리 테스트")
+    public void CreateClassroomWithShcool_And_LazyTest(){
+        // 반 생성
+        System.out.println("step1. classroom 생성 시작");
+        String classroom_name = "classroom1";
+        RequestCreateClassroomDTO requestCreateClassroomDTO = new RequestCreateClassroomDTO();
+        requestCreateClassroomDTO.setSchool_name("");
+        requestCreateClassroomDTO.setClassroom_name(classroom_name);
+        assertDoesNotThrow(() -> classroomService.CreateClassroom(requestCreateClassroomDTO));
+        System.out.println("step1. classroom 생성 완료\n");
+
+        // 학교 생성
+        System.out.println("step2. school 생성 시작");
+        String school_name = "school1";
+        Assertions.assertDoesNotThrow(() -> schoolService.CreateSchool(school_name));
+
+        RequestAddSchoolFromClassroomDto requset_dto = new RequestAddSchoolFromClassroomDto();
+        requset_dto.setClassroom_name(classroom_name);
+        requset_dto.setSchool_name(school_name);
+
+        /***
+         * update 쿼리문 실행
+         * Hibernate:
+         *     update
+         *         classroom
+         *     set
+         *         name=?,
+         *         school_id=?
+         *     where
+         *         classroom_id=?
+         */
+        assertDoesNotThrow(() -> classroomService.AddSchool(requset_dto));
+        System.out.println("step2. school 생성 완료\n");
+
+        System.out.println("step3. 반 검색 시작");
+        /***
+         * 학교이름을 찾으면 반을 찾는 select문이 실행될까? -> NO. 연관관계가 Lazy여서..
+         *
+         * LAZY 쿼리
+         * 
+         * Hibernate:
+         *     select
+         *         school0_.school_id as school_i1_1_,
+         *         school0_.name as name2_1_
+         *     from
+         *         school school0_
+         *     where
+         *         school0_.name=?
+         * 
+         */
+        ClassRoom find_classroom = classroomService.findByName(classroom_name);
+
+        System.out.println("step3. 반 검색 종료\n");
+    }
 }
