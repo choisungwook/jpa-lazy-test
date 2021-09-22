45bd0459590d52237ca2bb9c7e86df0b5bb23470
choisungwook <kgg1959@naver.com>
2021-09-22 17:16:17 +0900

    classroom 테스트코드 수정

--- backend/src/test/java/com/sungwook/lazytest/controller/ClassroomControllerTest.java
+++ backend/src/test/java/com/sungwook/lazytest/controller/ClassroomControllerTest.java
@@ -2,6 +2,8 @@ package com.sungwook.lazytest.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sungwook.lazytest.controller.dto.request.RequestCreateClassroomDTO;
+import com.sungwook.lazytest.repository.ClassroomRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,9 +20,14 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @AutoConfigureMockMvc
 class ClassroomControllerTest {
+    @Autowired MockMvc mockMvc;
 
-    @Autowired
-    MockMvc mockMvc;
+    @Autowired ClassroomRepository classroomRepository;
+
+    @AfterEach
+    public void AfterEach(){
+        classroomRepository.deleteAllInBatch();
+    }
 
     @Test
     @DisplayName("classroom 생성")
@@ -36,11 +43,27 @@ class ClassroomControllerTest {
                     .content(objectMapper.writeValueAsString(requestCreateClassroomDTO)))
                 .andExpect(status().isOk())
                 .andDo(print());
+    }
+
+    @Test
+    @DisplayName("classroom 중복생성")
+    public void CreateClassroomDuplicate() throws Exception {
+        String classroom_name = "classroom1";
+        RequestCreateClassroomDTO requestCreateClassroomDTO = new RequestCreateClassroomDTO();
+        requestCreateClassroomDTO.setClassroom_name(classroom_name);
+
+        ObjectMapper objectMapper = new ObjectMapper();
 
         mockMvc.perform(post("/api/v1/classroom/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(requestCreateClassroomDTO)))
                 .andExpect(status().isOk())
+                .andDo(print());
+
+        mockMvc.perform(post("/api/v1/classroom/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestCreateClassroomDTO)))
+                .andExpect(status().is4xxClientError())
                 .andDo(print());
     }
 }
