f2eadeb16483dd7ebc0166fa76ac9f831f490956
choisungwook <kgg1959@naver.com>
2021-09-23 20:45:13 +0900

    프론트엔드 테스트 버튼 추가

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <v-container fluid>
+    <v-btn class="blue white" elevation="2" outlined x-large @click="elb_healthcheck">
+      elb healthcheck
+    </v-btn>
     <v-btn class="blue white" elevation="2" outlined x-large @click="healthcheck">
-      healthcheck
+      normal healthcheck
     </v-btn>
     <h1>This is Home.</h1>
   </v-container>
@@ -14,6 +17,28 @@ export default {
   name: "Home",
 
   methods: {
+    elb_healthcheck() {
+      axios
+        .get(
+          process.env.VUE_APP_ENDPOINT + "/health",
+          {},
+          {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json; charset = utf-8",
+          }
+        )
+        .then((response) => {
+          console.log("endpoint -> " + process.env.VUE_APP_ENDPOINT);
+          console.log(response.data);
+          console.log("ping success");
+          console.log("\n");
+        })
+        .catch((error) => {
+          console.log("endpoint -> " + process.env.VUE_APP_ENDPOINT);
+          console.log("ping failed");
+          console.log(error);
+        });
+    },
     healthcheck() {
       axios
         .get(
