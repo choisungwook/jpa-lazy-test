038f7d4406645b2153e252f9ec6d49a3fa4c70bd
choisungwook <kgg1959@naver.com>
2021-09-23 18:40:26 +0900

    프론트엔드 ping버튼 추가

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,33 @@
 <template>
   <v-container fluid>
+    <v-btn class="blue white" elevation="2" outlined x-large @click="healthcheck">
+      healthcheck
+    </v-btn>
     <h1>This is Home.</h1>
   </v-container>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Home",
+
+  methods: {
+    healthcheck() {
+      axios
+        .get("/api/health")
+        .then((response) => {
+          console.log(response);
+          console.log(response.data);
+          console.log("ping success");
+          console.log("\n");
+        })
+        .catch((error) => {
+          console.log("ping failed");
+          console.log(error);
+        });
+    },
+  }
 };
 </script>
