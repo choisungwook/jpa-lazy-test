4e18b27e53e5c6e281b042ad56c75095ecc297d6
choisungwook <kgg1959@naver.com>
2021-09-25 20:12:19 +0900

    디버그 로그

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -7,6 +7,8 @@ if (process.env.ROUTER_PREFIX === undefined) {
   url = process.env.VUE_APP_ENDPOINT + "/" + process.env.ROUTER_PREFIX;
 }
 
+console.log("url -> " + url);
+
 const instance = axios.create({
   baseURL: url,
 });
