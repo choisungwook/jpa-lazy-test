425c7adbf701994337d554a78b471270f7dbadff
choisungwook <kgg1959@naver.com>
2021-09-25 20:16:34 +0900

    디버그 로그

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 
 let url = "";
+
+console.log("process.env.VUE_APP_ENDPOINT -> " + process.env.VUE_APP_ENDPOINT);
+console.log("process.env.ROUTER_PREFIX -> " + process.env.ROUTER_PREFIX);
 if (process.env.ROUTER_PREFIX === undefined) {
   url = process.env.VUE_APP_ENDPOINT;
 } else {
-  url = process.env.VUE_APP_ENDPOINT + "/" + process.env.ROUTER_PREFIX;
+  console.log("router_prefix is undefined");
+  url = process.env.ROUTER_PREFIX;
 }
 
 console.log("url -> " + url);
