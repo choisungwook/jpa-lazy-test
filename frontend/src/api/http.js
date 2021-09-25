8a0a22d621aa39b2d38fe87cecf34a1b08ffef5c
choisungwook <kgg1959@naver.com>
2021-09-25 20:30:29 +0900

    fix: router_prefix 수정

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -3,12 +3,15 @@ import axios from "axios";
 let url = "";
 
 console.log("process.env.VUE_APP_ENDPOINT -> " + process.env.VUE_APP_ENDPOINT);
-console.log("process.env.ROUTER_PREFIX -> " + process.env.ROUTER_PREFIX);
-if (process.env.ROUTER_PREFIX === undefined) {
+console.log(
+  "process.env.VUE_APP_ROUTER_PREFIX -> " + process.env.VUE_APP_ROUTER_PREFIX
+);
+if (process.env.VUE_APP_ROUTER_PREFIX === undefined) {
+  console.log("VUE_APP_ROUTER_PREFIX is undefined");
   url = process.env.VUE_APP_ENDPOINT;
 } else {
-  console.log("router_prefix is undefined");
-  url = process.env.ROUTER_PREFIX;
+  console.log("VUE_APP_ROUTER_PREFIX is !!!");
+  url = process.env.VUE_APP_ROUTER_PREFIX;
 }
 
 console.log("url -> " + url);
