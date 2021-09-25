63513511aebb61b769cc5e4fb9a9d54f929575cd
choisungwook <kgg1959@naver.com>
2021-09-25 20:57:26 +0900

    nginx 주소 설정

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -1,23 +1,14 @@
 import axios from "axios";
 
-// let url = "";
-
-// console.log("process.env.VUE_APP_ENDPOINT -> " + process.env.VUE_APP_ENDPOINT);
-// console.log(
-//   "process.env.VUE_APP_ROUTER_PREFIX -> " + process.env.VUE_APP_ROUTER_PREFIX
-// );
-// if (process.env.VUE_APP_ROUTER_PREFIX === undefined) {
-//   console.log("VUE_APP_ROUTER_PREFIX is undefined");
-//   url = process.env.VUE_APP_ENDPOINT;
-// } else {
-//   console.log("VUE_APP_ROUTER_PREFIX is !!!");
-//   url = process.env.VUE_APP_ROUTER_PREFIX;
-// }
-
-// console.log("url -> " + url);
+let url = "";
+if (process.env.VUE_APP_ROUTER_PREFIX === undefined) {
+  url = process.env.VUE_APP_ENDPOINT;
+} else {
+  url = process.env.VUE_APP_ROUTER_PREFIX;
+}
 
 const instance = axios.create({
-  baseURL: process.env.VUE_APP_ENDPOINT,
+  baseURL: url,
 });
 
 export default instance;
